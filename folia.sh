7dd7ca755c77c2fec173e66e087857297eca52eb
gickhub <gicquel.pierreyves@gmail.com>
2018-02-18 17:57:12 +0100

    chan

--- folia.sh
+++ folia.sh
@@ -1,5 +1,5 @@
 export LD_LIBRARY_PATH=/usr/local/lib
-readonly foliaPath=/usr/src/app
+readonly foliaPath=/usr/src/app/Debian8_x64
 readonly foliaExec=./Algo_LIRIS_Debian_x64
 cd $foliaPath
 $foliaExec $1 $2 $3 $4
