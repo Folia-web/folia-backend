5162aeca45f8ecd6b26edabcc376f6fa7361db3f
gick <gicquel.pierreyves@gmail.com>
2018-05-29 16:17:27 +0200

    Making server RESTFULL

--- app.js
+++ app.js
@@ -8,19 +8,12 @@ const fileUpload = require('express-fileupload');
 var app = express();
 var port = 8081;
 app.use(morgan('dev')); // log every request to the console
-
 app.use(fileUpload());
-//Grid.mongo = mongoose.mongo;
-// configuration ===============================================================
 
 
 
 
 
 
-// routes ======================================================================
 require('./route/routes.js')(app); // load our routes and pass in our app and fully configured passport
-//require('./app/route/imageAnalysisRoute.js')(app, gfs,passport);
-
-// launch ======================================================================
 app.listen(port);
--- route/routes.js
+++ route/routes.js
@@ -1,106 +1,79 @@
 module.exports = function (app) {
   var folia = require('../analysis')
   var fs = require('fs')
-    // normal routes ===============================================================
+  // normal routes ===============================================================
 
-    //###################################################################
-    //--------------- UTILITY FUNCTIONS
-    //###################################################################
-    app.post('/images', function (req, res) {
-      if(!req.files){
-        return res.status(400).send('No file uploaded')
-      }
-        let leafId = req.body.leafId
-        let leaf=req.files.leaf
+  //###################################################################
+  //--------------- UTILITY FUNCTIONS
+  //###################################################################
+  app.post('/images', function (req, res) {
+    if (!req.files) {
+      return res.status(400).send('No file uploaded')
+    }
+    let leafId = req.body.leaf._id
+    let leaf = req.files.leaf
 
-        if(!leafId || !leaf){
-          return res.status(400).send('Leaf parameters incorrect')
-        }
+    if (!leafId || !leaf) {
+      return res.status(400).send('Leaf parameters incorrect')
+    }
 
-        let coloriageId = req.body.coloriageId
-        let coloriage=req.files.coloriage
+    let base64Image = req.body.coloriage.base64Image.replace(/^data:image\/png;base64,/, "");
 
-        if(!coloriage || !coloriageId){
-          return res.status(400).send('Coloriage parameters incorrect')
-        }
+    if (!coloriage) {
+      return res.status(400).send('Coloriage parameters incorrect')
+    }
 
 
-        leaf.mv('/tmp/' + leafId)
-        .then(function(){
-          coloriage.mv('/tmp/' + coloriageId + '.png')
-        })
-        .then(function(){
-          return res.status(200).send({operation:'upload',success:true})
-        })
-        .catch(function(err){
-          console.log(err)
-          return res.status(500).send({operation:'upload',success:false,message:'Problem check write access?'})
+    leaf.mv('/tmp/' + leafId)
+      .then(function () {
+
+        fs.writeFile("/tmp/"+leafId+"color.png", base64Data, 'base64', function (err) {
+          console.log(err);
         });
-
-
-
-    });
-
-
-    app.get('/mask/:id',function(req,res){
-      res.download('/tmp/' + req.params.id + '.png')
-    })
-
-    app.get('/stringResult/:id',function(req,res){
-      fs.readFile('/tmp/'+req.params.id+'.csv','utf8',(err,data)=>{
-        if(err){
-          return res.status(500).send('FIle path problem')
-        }
-        res.send(data)
       })
-    })
+      .then(function () {
+        return res.status(200).send({ operation: 'upload', success: true })
+      })
+      .catch(function (err) {
+        console.log(err)
+        return res.status(500).send({ operation: 'upload', success: false, message: 'Problem check write access?' })
+      });
+  });
 
-    app.get('/result/:id',function(req,res){
-      res.download('/tmp/' + req.params.id + '.csv')
-    })
 
-    // handle media posted by authenticated users
-    app.post('/leafimage', function (req, res) {
-      if(!req.files){
-        return res.status(400).send('No file uploaded')
+  app.get('/mask/:id', function (req, res) {
+    res.download('/tmp/' + req.params.id + '.png')
+  })
+
+  app.get('/stringResult/:id', function (req, res) {
+    fs.readFile('/tmp/' + req.params.id + '.csv', 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).send('FIle path problem')
       }
-        let id = req.body.id
-        let leaf=req.files.leaf
-        leaf.mv('/tmp/' +id,function(err){
-          if(err)
-            return res.status(500).send(err)
+      res.send(data)
+    })
+  })
 
-          res.send('upload success')
-        })
+  app.get('/result/:id', function (req, res) {
+    res.download('/tmp/' + req.params.id + '.csv')
+  })
 
+  // handle media posted by authenticated users
+
+
+
+  app.get('/result', function (req, res) {
+    res.writeHead(200, {
+      "Content-Type": "text/event-stream",
+      "Cache-control": "no-cache"
     });
+    if (req.query.leafId && req.query.maskId) {
+      let leaf = req.query.leafId
+      let mask = req.query.maskId
+      folia.analysis(leaf, mask, res)
 
-    app.post('/maskimage', function (req, res) {
-      if(!req.files){
-        return res.status(400).send('No file uploaded')
-      }
-        let id = req.body.id
-        let mask = req.files.mask;
-        mask.mv('/tmp/' + id,function(err){
-          if(err)
-            return res.status(500).send(err)
-
-          res.send('upload success')
-        })
-
-    });
-
-
-    app.get('/result',function(req,res){
-      res.writeHead(200, { "Content-Type": "text/event-stream",
-                           "Cache-control": "no-cache" });
-      if(req.query.leafId && req.query.maskId){
-        let leaf=req.query.leafId
-        let mask = req.query.maskId
-        folia.analysis( leaf, mask,res)
-
-      }
-    })
+    }
+  })
 
 
 }
