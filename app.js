663e368ed1fd52ff566c2118cdd684a993f8f04e
gick <gicquel.pierreyves@gmail.com>
2018-05-30 13:44:53 +0200

    changes

--- app.js
+++ app.js
@@ -4,14 +4,23 @@
 // get all the tools we need
 var express = require('express');
 var morgan=require('morgan')
+var firebase = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
 const fileUpload = require('express-fileupload');
 var app = express();
 var port = 8081;
 app.use(morgan('dev')); // log every request to the console
 app.use(fileUpload());
+firebase.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://rev-game-6e36a.firebaseio.com"
+  });
+var db =firebase.database()
+var users=db.ref('users/')
 
-
-
+users.once('value').then(function(snap){
+    console.log(snap.val())
+})
 
 
 
